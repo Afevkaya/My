01bd2b59f5f7e96732a047239041f3b8324c8bb8
ahmet.evkaya <ahmet.evkaya@hangikredi.com>
2024-06-15 13:00:10 +0300

    150624 / FeautureList ViewComponent's To Edit

--- MyBlog.UI/ViewComponents/Feature/FeatureListViewComponent.cs
+++ MyBlog.UI/ViewComponents/Feature/FeatureListViewComponent.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using MyBlog.Core.Services;
 
 namespace MyBlog.UI.ViewComponents.Feature;
 
 public class FeatureListViewComponent: ViewComponent
 {
-    public IViewComponentResult Invoke()
+    private readonly IFeatureService _featureService;
+
+    public FeatureListViewComponent(IFeatureService featureService)
     {
-        return View();
+        _featureService = featureService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var features = await _featureService.GetsAsync();
+        return View(features.Data);
     }
 }
