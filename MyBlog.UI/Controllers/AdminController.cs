88b93abf3d8b135a3cdec7846c3f4400e079ae11
ahmet.evkaya <ahmet.evkaya@hangikredi.com>
2024-06-17 22:24:32 +0300

    170624 / Splitting _AdminLayout into Partials

--- MyBlog.UI/Controllers/AdminController.cs
+++ MyBlog.UI/Controllers/AdminController.cs
@@ -4,8 +4,27 @@ namespace MyBlog.UI.Controllers;
 
 public class AdminController : Controller
 {
-    
     public PartialViewResult PartialSideBar()
+    {
+        return PartialView();
+    }
+
+    public PartialViewResult PartialFooter()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialNavBar()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialHead()
+    {
+        return PartialView();
+    }
+    
+    public PartialViewResult PartialScript()
     {
         return PartialView();
     }
